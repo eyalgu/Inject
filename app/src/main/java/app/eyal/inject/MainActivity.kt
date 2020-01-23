03579a6257ed941753ce6df7281b65197f3f7520
Eyal Guthmann <eyalgu@users.noreply.github.com>
2020-01-23 14:49:56 -0800

    create graph when user is available

--- app/src/main/java/app/eyal/inject/MainActivity.kt
+++ app/src/main/java/app/eyal/inject/MainActivity.kt
@@ -3,17 +3,28 @@ package app.eyal.inject
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import app.eyal.appentity.App
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var appGraph: AppGraph
+    var userGraph: UserGraph? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val appGraph = DaggerAppGraph.factory().create(App)
-        val userGraph = appGraph.userGraphFactory.create(appGraph.appComponentB.appDepB.user)
-        val userDepB = userGraph.userComponentB.userDepB
-        Log.d("MainActivity", userDepB.toString())
+        appGraph = DaggerAppGraph.factory().create(App)
+        lifecycleScope.launchWhenCreated {
+            appGraph.appComponentB.appDepB.user.collect {
+                userGraph = appGraph.userGraphFactory.create(it).also {
+                    val userDepB = it.userComponentB.userDepB
+                    Log.d("MainActivity", userDepB.toString())
+                }
+
+            }
+        }
     }
 }
--- appModuleB/src/main/java/app/eyal/appmoduleb/AppDepB.kt
+++ appModuleB/src/main/java/app/eyal/appmoduleb/AppDepB.kt
@@ -2,9 +2,10 @@ package app.eyal.appmoduleb
 
 import app.eyal.appmodulea.AppDepA
 import app.eyal.usermodel.User
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 data class AppDepB @Inject constructor(private val appDepA: AppDepA) {
-    val user = User("123")
+    val user = flowOf(User("123"))
 }
 
